978ea22e74d692ce3bfd3d434805fb19e880d1c6
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-09-18 17:01:39 +0400

    add MobileNav component

---
+++ src/components/navigation/MobileNav/MobileNav.styled.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { DrawerContent } from "@chakra-ui/react";
+
+export const SMobileNav = styled.nav`
+  display: block;
+
+  @media (min-width: 1440px) {
+    display: none;
+  }
+`;
+
+export const SBurgerButton = styled.button.attrs({
+  type: "button",
+})`
+  width: 22px;
+  height: 16.343px;
+  flex-shrink: 0;
+  background-color: transparent;
+`;
+
+export const SDrawerContent = styled(DrawerContent)`
+  && {
+    background-color: hsla(244, 100%, 15%, 0.8);
+    color: white;
+    height: 100vh;
+    width: 75% !important;
+    padding: 8.5rem 3.7rem;
+
+    .chakra-drawer__close-btn {
+      color: white;
+      font-size: 1.5rem;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+    @media (min-width: 1440px) {
+      display: none;
+    }
+  }
+`;
