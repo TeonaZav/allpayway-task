2a94f1501601d29c8a70a7fc40167c0d5d5ab6ab
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-09-18 16:00:02 +0400

    add NavList component

---
+++ src/components/navigation/Navlist/Navlist.styled.ts
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { NavLinkProps } from "react-router-dom";
+
+export const SNavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 5.3rem;
+
+  @media (min-width: 1440px) {
+    flex-direction: row;
+    align-items: center;
+    gap: 3.8rem;
+  }
+`;
+
+export const SNavLink = styled(NavLink)<NavLinkProps>`
+  font-family: inherit;
+  font-size: var(--fs-medium);
+  color: var(--clr-links);
+  font-weight: 500;
+  text-decoration: none;
+
+  &.active {
+    color: var(--clr-links-active);
+  }
+`;
---
+++ src/components/navigation/Navlist/Navlist.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { SNavList, SNavLink } from "./Navlist.styled";
+
+interface NavItem {
+  label: string;
+  to: string;
+  end?: boolean;
+}
+
+const navItems: NavItem[] = [
+  { label: "Home", to: "/", end: true },
+  { label: "Services", to: "/services" },
+  { label: "Projects", to: "/projects" },
+  { label: "Partners", to: "/partners" },
+  { label: "About Us", to: "/about" },
+  { label: "Contact", to: "/contact" },
+];
+
+const getActiveClass = ({ isActive }: { isActive: boolean }) =>
+  isActive ? "active" : "";
+
+const Navlist: React.FC = () => {
+  return (
+    <SNavList>
+      {navItems.map((item) => (
+        <li key={item.to}>
+          <SNavLink to={item.to} end={item.end} className={getActiveClass}>
+            {item.label}
+          </SNavLink>
+        </li>
+      ))}
+    </SNavList>
+  );
+};
+
+export default Navlist;
