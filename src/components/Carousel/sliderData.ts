088672ec22ef3e1d96b6381f327ddd1cad1b783b
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-09-19 09:25:03 +0400

    add carousel component

---
+++ src/components/Carousel/sliderData.ts
@@ -0,0 +1,6 @@
+import slide1 from "./../../assets/images/slide-1.png";
+import slide2 from "./../../assets/images/slide-2.png";
+import slide3 from "./../../assets/images/slide-3.png";
+import slide4 from "./../../assets/images/slide-1.png";
+
+export const sliderData = [slide1, slide2, slide3, slide4];
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,14 @@ import ServiceList from "./services/ServiceList/ServiceLIst";
 import ServiceCard from "./services/ServiceCard/ServiceCard";
 import TextBox from "./TextBox/TextBox";
 import HomepageContent from "./HompageContent/HomepageContent";
+import Carousel from "./Carousel/Carousel";
 
-export { Layout, Loading, ServiceList, ServiceCard, TextBox, HomepageContent };
+export {
+  Layout,
+  Loading,
+  ServiceList,
+  ServiceCard,
+  TextBox,
+  HomepageContent,
+  Carousel,
+};
